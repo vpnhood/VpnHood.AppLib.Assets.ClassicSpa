447bc766193aa8d92e0599db7af7c57358b27961
Trudy <trudy@vpnhood.com>
2025-03-02 13:19:06 -0800

    Publish

--- _publish.ps1
+++ _publish.ps1
@@ -1,9 +1,15 @@
 ﻿$solutionDir = $PSScriptRoot;
 
+# get the latest version
 $gitDir = "$solutionDir/.git";
 git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish";
 git --git-dir=$gitDir --work-tree=$solutionDir pull;
+
+# bump version
+. "$PSScriptRoot/pub/VersionBump.ps1" -versionFile "$PSScriptRoot/pub/PubVersion.json" -bump 1;
+
+# push the new version
 git --git-dir=$gitDir --work-tree=$solutionDir push;
 
 # swtich to main branch 681
-git --git-dir=$gitDir push origin dev:main --force
+git --git-dir=$gitDir push origin dev:main
