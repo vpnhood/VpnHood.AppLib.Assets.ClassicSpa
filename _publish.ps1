69ec5c858a4113401e5947cc75d4cc11536d3a89
Trudy <trudy@vpnhood.com>
2025-03-02 13:50:43 -0800

    Publish

--- _publish.ps1
+++ _publish.ps1
@@ -8,20 +8,21 @@ $solutionDir = $PSScriptRoot;
 $projectFile = "$solutionDir/VpnHood.AppLib.Assets.ClassicSpa/VpnHood.AppLib.Assets.ClassicSpa.csproj";
 
 # get the latest version
+Write-Host "Commit and get pull the lastest" -ForegroundColor Orange;
 $gitDir = "$solutionDir/.git";
 git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish";
 git --git-dir=$gitDir --work-tree=$solutionDir pull;
 
 # bump version
+Write-Host "Increasing version" -ForegroundColor Orange;
 . "$PSScriptRoot/pub/VersionBump.ps1" -versionFile "$PSScriptRoot/pub/PubVersion.json" -bump $bump;
-# update project version
 UpdateProjectVersion($projectFile);
 
 # push the new version
+Write-Host "Push to current branch" -ForegroundColor Orange;
 git --git-dir=$gitDir --work-tree=$solutionDir push;
 
-# swtich to main branch
+# psuh to build
+Write-Host "Pushing to main branch" -ForegroundColor Orange;
 $branch = $IsRelease -eq "1" ? "main" : "prerelease";
-echo $branch
-exit 
 git --git-dir=$gitDir push origin dev:$branch
