02cdc4927c1fe6cd9ea78fb0c88b9a82722086b0
Trudy <trudy@vpnhood.com>
2025-03-02 12:48:31 -0800

    publish

---
+++ _publish.ps1
@@ -0,0 +1,9 @@
+﻿$solutionDir = $PSScriptRoot;
+
+$gitDir = "$solutionDir/.git";
+git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish";
+git --git-dir=$gitDir --work-tree=$solutionDir pull;
+git --git-dir=$gitDir --work-tree=$solutionDir push;
+
+# swtich to main branch
+git --git-dir=$gitDir push origin Development:main
