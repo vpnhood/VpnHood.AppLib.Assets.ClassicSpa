0187a6fc00f1d0a0bdb8f84d535c0d0852df9a9c
Trudy <trudy@vpnhood.com>
2025-03-02 01:59:03 -0800

    Add ConnectSpaResources class for UI resource management
    Introduces a new static class `ConnectSpaResources` in the
    `VpnHood.AppLib.Assets.ClassicSpa` namespace. This class
    provides static properties for UI-related resources, including
    colors for the navigation bar, window background, and progress
    bar, as well as byte arrays for system tray icons for various
    connection states. Adds `using System.Drawing;` for color
    definitions.

---
+++ VpnHood.AppLib.Assets.ClassicSpa/ConnectSpaResources.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace VpnHood.AppLib.Assets.ClassicSpa;
+
+public static class ConnectSpaResources 
+{
+    public static byte[] SpaZipData => Resources.Spa;
+    public static Color NavigationBarColor => Color.FromArgb(21, 14, 61);
+    public static Color WindowBackgroundColor => Color.FromArgb(21, 14, 61);
+    public static Color ProgressBarColor => Color.FromArgb(231, 180, 129);
+    public static byte[] SystemTrayConnectedIcon => Resources.VpnConnectedIcon;
+    public static byte[] SystemTrayConnectingIcon => Resources.VpnConnectingIcon;
+    public static byte[] SystemTrayDisconnectedIcon => Resources.VpnDisconnectedIcon;
+}
